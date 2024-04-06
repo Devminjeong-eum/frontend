a0eaf586d6a4b0fef49c4dc7dbb5f4e88815d3aa
nyoung113 <mysoulk0113@gmail.com>
2024-04-07 00:51:01 +0900

    feat: detail page 생성
    디테일 페이지를 생성했습니다. params로 받아서 wordId 데이터를 가져옵니다.

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+interface WordDetail {
+  wordId: number; // id
+  wordName: string; // 단어 제목
+  wordDescription: string; // 단어 설명
+  wordSpeak: string; // 올바른 발음 리스트
+  wrongSpeak: string; // 틀린 발음 리스트
+  wordExamples: string; // 예문 리스트 ($로 구분)
+}
+
+export default function Detail() {
+  const { wordId } = useParams();
+
+  const data = useQuery<WordDetail>({
+    queryFn: () =>
+      new Promise((resolve) => {
+        resolve({
+          wordId: 1,
+          wordName: 'AJAX',
+          wordDescription:
+            '개발용어의 정의가 들어가는 부분입니다. 개발용어의 정의가 들어가는 부분입니다. 개발용어의 정의가 들어가는 부분입니다. 개발용어의 정의임.',
+          wordSpeak: '에이잭스',
+          wrongSpeak: '아작스, 아약스, 에이작스, 에작스',
+          wordExamples:
+            '개발용어의 정의가 들어가는 부분입니다. 개발용어의 정의가 들어가는 부분입니다.$개발용어의 정의가 들어가는 부분입니다. 개발용어의 정의가 들어가는 부분입니다. ',
+        });
+      }),
+    queryKey: ['get_wordDetail', wordId],
+  });
+
+  return (
+    <div>
+      <header className="w-full">개발용어</header>
+      <h1>AJAX</h1>
+    </div>
+  );
+}
