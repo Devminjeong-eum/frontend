1c11bc8e8ee757e20458f5975aceb0a3111bac6c
uk11 <dlckddnrr@gmail.com>
2024-04-08 05:18:48 +0900

    feat: 퀴즈 기능 추가

---
+++ src/pages/Quiz/StartQuiz.tsx
@@ -0,0 +1,102 @@
+import { useState } from 'react';
+import { quizData } from '@/pages/Quiz/quizData';
+import OSVG from '@/components/svgComponent/OSVG';
+import XSVG from '@/components/svgComponent/XSVG';
+
+export default function StartQuiz() {
+  const [currentQuiz, setCurrentQuiz] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [selectOption, setSelectOption] = useState<string | null>(null);
+
+  const progress = ((currentQuiz + 1) / quizData.length) * 100;
+
+  const nextQuiz = () => {
+    setTimeout(() => {
+      if (currentQuiz < quizData.length - 1) {
+        setCurrentQuiz(currentQuiz + 1);
+        setSelectOption(null);
+      } else {
+        setShowScore(true);
+      }
+    }, 1000);
+  };
+
+  const handleAnswerOptionClick = (selectedOption: string) => {
+    if (selectedOption === quizData[currentQuiz].correctAnswer) {
+      setScore(score + 1);
+    }
+    setSelectOption(selectedOption);
+    nextQuiz();
+  };
+
+  const restartQuiz = () => {
+    setCurrentQuiz(0);
+    setScore(0);
+    setShowScore(false);
+  };
+
+  return (
+    <div className="flex justify-center min-h-screen">
+      <div className="flex flex-col items-center w-full max-w-[430px] border-1 border-x border-gray-200 shadow-xl">
+        {showScore ? (
+          <div className="">
+            {quizData.length} 문제 중 {score} 문제를 맞추셨습니다
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md mt-4 focus:outline-none focus:ring focus:ring-blue-500"
+              onClick={restartQuiz}
+            >
+              다시 풀기
+            </button>
+          </div>
+        ) : (
+          <div className="w-full flex flex-col justify-center items-center bg-[#fbfcfe]">
+            <header className="flex justify-center items-center h-[60px] font-bold">
+              TEST 중이에요.
+            </header>
+            <div className="w-full bg-[#ECEFF5] flex">
+              <div
+                className="bg-[#0C3FC1] h-[3px]"
+                style={{ width: `${progress}%` }}
+              ></div>
+            </div>
+            <div className="flex flex-col items-center">
+              <p className="text-[#5E5E5E] mb-2">아래 단어의 발음은?</p>
+              <div className=" text-[30px] font-bold mb-8">
+                {quizData[currentQuiz].question}
+              </div>
+            </div>
+            <div className="flex flex-col items-center w-full">
+              {quizData[currentQuiz].options.map((option) => (
+                <button
+                  key={option}
+                  className={`w-[90%] font-bold bg-white ring-1 ring-[#F2F4F9] h-[48px] rounded-[16px] mb-4 ${
+                    selectOption === option &&
+                    (option === quizData[currentQuiz].correctAnswer
+                      ? 'bg-[#0C3FC1] text-white'
+                      : 'bg-[#912828] text-white')
+                  }`}
+                  onClick={() => handleAnswerOptionClick(option)}
+                >
+                  <div className="flex justify-center items-center relative">
+                    {selectOption === option &&
+                      (selectOption === quizData[currentQuiz].correctAnswer ? (
+                        <div className="absolute left-4">
+                          <OSVG />
+                        </div>
+                      ) : (
+                        <div className="absolute left-4 top-1">
+                          <XSVG />
+                        </div>
+                      ))}
+                    {option}
+                  </div>
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Quiz/index.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+import ScoreSVG from '@/components/svgComponent/ScoreSvg';
+import BackSpaceSVG from '@/components/svgComponent/BackSpaceSvg';
+
+export default function Quiz() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex justify-center min-h-screen">
+      <div className="flex flex-col justify-center items-center w-full max-w-[430px] border-1 border-x border-gray-200 shadow-xl bg-main-gradiant-top h-13 relative">
+        <header
+          className="absolute top-7 left-5 cursor-pointer"
+          onClick={() => navigate('/')}
+        >
+          <BackSpaceSVG />
+        </header>
+        <div className="absolute top-12 right-0">
+          <ScoreSVG />
+        </div>
+        <div className="flex flex-col items-center mb-[100px]">
+          <div className="mb-8 font-test text-white text-[40px] text-center w-[190px]">
+            개발 용어 발음 테스트
+          </div>
+          <p className="text-white">나의 개발 용어 발음 지식은?</p>
+        </div>
+        <button
+          onClick={() => navigate('/startQuiz')}
+          className="w-[90%] h-[48px] rounded-[16px] absolute bottom-10 bg-white/20 ring-1 ring-white/40 focus:ring-white/60 outline-none text-white"
+        >
+          테스트 시작하기
+        </button>
+      </div>
+    </div>
+  );
+}
