4006517c2b53f093e265469292e69d613735ff68
nyoung113 <mysoulk0113@gmail.com>
2024-04-07 15:57:34 +0900

    feat: SVG 컴포넌트 생성
    필요한 SVG 컴포넌트들 import

---
+++ src/components/svgComponent/CorrectSvg.tsx
@@ -0,0 +1,13 @@
+export default function CorrectSvg() {
+  return (
+    <svg
+      width="11"
+      height="11"
+      viewBox="0 0 11 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="5.5" cy="5.5" r="4.75" stroke="#40FF53" stroke-width="1.5" />
+    </svg>
+  );
+}
