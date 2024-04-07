3cf24d1b146cdb64736cddc7a0ac9c7788939bc1
uk11 <dlckddnrr@gmail.com>
2024-04-08 05:19:19 +0900

    feat: SVG 추가

---
+++ src/components/svgComponent/BackSpaceSvg.tsx
@@ -0,0 +1,23 @@
+export default function BackSpaceSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      x="1"
+      y="2"
+    >
+      <g opacity="0.7">
+        <path
+          d="M15 5L8 12L15 19"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </svg>
+  );
+}
