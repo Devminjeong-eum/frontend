f49b64a7bab78235d0270d95b4c6f534c99387b5
nyoung113 <mysoulk0113@gmail.com>
2024-04-07 16:03:14 +0900

    feat: 필요 없는 푸터 제거와 백그라운드 색 적용
    푸터가 없어서 제거했고, 백그라운드 색을 적용

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,14 +1,12 @@
 import Header from './Header.tsx';
-import Footer from './Footer.tsx';
 import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="flex justify-center min-h-screen ">
+    <div className="flex justify-center min-h-screen bg-[#FBFCFE]">
       <div className="w-full max-w-[430px] border-1 border-x border-gray-200 shadow-xl">
         <Header />
         <Outlet />
-        <Footer />
       </div>
     </div>
   );
