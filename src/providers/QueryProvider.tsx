2bf3d7fe5d1f6fce4160b44331d1f37f7ce98399
nyoung113 <mysoulk0113@gmail.com>
2024-04-24 22:47:53 +0900

    feat: react-query ssr 기본 세팅
    provider 세팅했습니다.

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,11 +1,33 @@
 'use client';
-
-import React, { PropsWithChildren, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
 
-function QueryProvider({ children }: PropsWithChildren) {
-  const [client] = useState(new QueryClient());
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
 }
 
-export default QueryProvider;
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === 'undefined') {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function QueryProvider({ children }: PropsWithChildren) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
