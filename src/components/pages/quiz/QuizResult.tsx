1ddcda29ed5662609682c9453bb20897ad9a00cc
uk11 <dlckddnrr@gmail.com>
2024-04-27 04:33:17 +0900

    feat: 퀴즈 점수 컴포넌트 분리

--- src/components/pages/quiz/QuizResult.tsx
+++ src/components/pages/quiz/QuizResult.tsx
@@ -1,7 +1,7 @@
 import BlackBackSpaceSVG from '@/components/svg-component/BlackBackSpaceSVG';
-import ScoreResultSvg from '@/components/svg-component/ScoreResultSvg';
 import { QUIZ_PATH } from '@/routes/path.ts';
 import Link from 'next/link';
+import QuizScore from './QuizScore';
 
 type QuizResultProps = {
   score: number;
@@ -18,23 +18,7 @@ export default function QuizResult({ score }: QuizResultProps) {
         </Link>
         <div className=" m-auto font-blod pr-3">TEST 결과</div>
       </header>
-      <div className="w-full aspect-square relative pt-4 flex justify-center">
-        <div className="absolute top-[35%] text-center">
-          <p>
-            <span className="font-semibold text-[#313140] text-[18px]">
-              사용자
-            </span>{' '}
-            님의 <br />
-            개발 용어 발음 실력은
-          </p>
-          <div
-            className={`font-gugi text-main-blue ${resultScore.toString().length > 2 ? `text-[64px]` : `text-[70px]`}`}
-          >
-            {resultScore}점
-          </div>
-        </div>
-        <ScoreResultSvg />
-      </div>
+      <QuizScore resultScore={resultScore} />
       <Link href={QUIZ_PATH}>
         <button className="bg-[#4057DB] rounded-[16px] mt-[28px] h-[52px] font-semibold text-white w-full">
           다시 도전하러 가기
