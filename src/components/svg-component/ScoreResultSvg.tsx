2facd86375761a6d409122cf2e2022959c32d744
uk11 <dlckddnrr@gmail.com>
2024-04-30 14:44:44 +0900

    feat: 퀴즈 결과 svg 크기 변경

--- src/components/svg-component/ScoreResultSvg.tsx
+++ src/components/svg-component/ScoreResultSvg.tsx
@@ -3,7 +3,7 @@ export default function ScoreResultSvg() {
     <svg
       width="full"
       height="full"
-      viewBox="0 0 328 311"
+      viewBox="0 0 321 294"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
