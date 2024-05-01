1e9b58bdb05182e24ead8aaac1875c24fa9fcc84
ono212 <qkrwjdals302@naver.com>
2024-05-01 22:27:14 +0900

    feat: 단어장 페이지 헤더 구현

---
+++ src/components/pages/my-wordbook/index.tsx
@@ -0,0 +1,9 @@
+import MyWordbookHeader from './MyWordbookHeader';
+
+export default function MyWordbook() {
+  return (
+    <div className="bg-main-gradiant-full">
+      <MyWordbookHeader />
+    </div>
+  );
+}
