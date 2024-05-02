5d0137c498214dd1363edee0c0cf4fca098ded4c
ono212 <qkrwjdals302@naver.com>
2024-05-03 06:18:05 +0900

    style: 전체 border가 스크롤 했을 때 끊기는 문제때문에 height제거, height 제거 후 단어장에 단어가 없을 때 스크롤이 생기는 문제때문에 padding-bottom 제거

--- src/components/pages/my-wordbook/index.tsx
+++ src/components/pages/my-wordbook/index.tsx
@@ -19,7 +19,7 @@ export default function MyWordbook() {
       <div className="bg-wordbook-gradient h-32">
         <MyWordbookHeader />
       </div>
-      <div className="h-[calc(100vh-8rem)] flex flex-col justify-between">
+      <div className="flex flex-col justify-between">
         <div className="relative bottom-16 flex flex-col">
           <div className="flex">
             <div className="w-[calc(46%+30px)] h-9 overflow-hidden rounded-tl-xl relative top-px">
@@ -55,7 +55,7 @@ export default function MyWordbook() {
             </div>
           )}
         </div>
-        <div className="flex flex-col items-center gap-8 relative top-[-2rem] pb-7">
+        <div className="flex flex-col items-center gap-8 relative top-[-2rem]">
           {totalItems === 0 ? (
             <Link
               href={WORD_LIST_PATH}
