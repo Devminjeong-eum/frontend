61bd88d1677fffceb68bc5eeb8b0c88e7b99c669
ono212 <qkrwjdals302@naver.com>
2024-05-02 19:00:11 +0900

    feat: 하단의 퀴즈 배너 구현 - gradient 2개 추가 - 헤더 backgroundImage 수정

--- src/components/pages/my-wordbook/index.tsx
+++ src/components/pages/my-wordbook/index.tsx
@@ -1,9 +1,22 @@
 import MyWordbookHeader from './MyWordbookHeader';
+import QuizBanner from './QuizBanner';
 
 export default function MyWordbook() {
   return (
-    <div className="bg-main-gradiant-full">
-      <MyWordbookHeader />
-    </div>
+    <>
+      <div className="bg-wordbook-gradient">
+        <MyWordbookHeader />
+        <div className="pt-3">
+          <div className="w-[calc(46%+30px)] h-9 overflow-hidden rounded-tl-xl relative top-px">
+            <div className="bg-[#FBFCFE] [transform:rotateY(180deg)_skew(-30deg)_translate(30px,0px)] h-full rounded-tl-md flex justify-center items-center">
+              <div className="[transform:rotateY(180deg)_skew(-30deg)_translate(-40px,0px)] text-[#313140] font-medium text-sm leading-[18px] tracking-[-0.02em] opacity-90">
+                총 00개
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <QuizBanner />
+    </>
   );
 }
