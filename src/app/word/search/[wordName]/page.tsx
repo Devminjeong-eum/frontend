c690b565f39168555f5c04c1059888289d11d007
uk11 <dlckddnrr@gmail.com>
2024-05-10 12:07:34 +0900

    comment: 쿼리 스트링 방식 주석 처리

--- src/app/word/search/[wordName]/page.tsx
+++ src/app/word/search/[wordName]/page.tsx
@@ -1,12 +1,16 @@
+// 쿼리스트링 사용 시 /app/search/page.tsx
+
 import Header from '@/components/layout/Header';
 import SearchClientPage from '@/components/pages/search';
 
 type Props = {
   params: { wordName: string };
+  // searchParams: { keyword: string };
 };
 
 export default async function SearchPage({ params }: Props) {
   const wordName = decodeURI(params.wordName);
+  // const wordName = decodeURI(searchParams.keyword);
 
   return (
     <>
--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -13,6 +13,7 @@ export default function SearchBar() {
   const navigateToSearch = (): void => {
     inputRef.current?.value.trim() &&
       router.push(`/word/search/${inputRef.current.value}`);
+    // router.push(`/word/search?keyword=${inputRef.current.value}`);
   };
 
   const handleSearchKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
