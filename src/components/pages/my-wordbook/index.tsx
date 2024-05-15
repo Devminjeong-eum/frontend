b209a9b99072a2b5d72a3e845290e9bd3bb20ff7
ono212 <qkrwjdals302@naver.com>
2024-05-15 17:49:43 +0900

    feat: 드롭다운 UI 구현

--- src/components/pages/my-wordbook/index.tsx
+++ src/components/pages/my-wordbook/index.tsx
@@ -9,6 +9,7 @@ import Pagination from '../../common/Pagination';
 import NoWordSvg from '@/components/svg-component/NoWordSvg';
 import Link from 'next/link';
 import { WORD_LIST_PATH } from '@/routes/path';
+import ArrowDownSvg from '@/components/svg-component/ArrowDownSvg';
 
 export default function MyWordbook() {
   const [current, setCurrent] = useState(1);
@@ -29,7 +30,28 @@ export default function MyWordbook() {
                 </div>
               </div>
             </div>
-            <div>저장한순</div>
+            <div className="relative inline-block ml-auto mr-5 group">
+              <div className="cursor-pointer">
+                <button className="w-[93px] text-white text-[15px] tracking-tight py-1 pr-1 flex items-center justify-end hover:bg-[#5360D4] focus:rounded-lg hover:rounded-lg">
+                  최신순
+                  <ArrowDownSvg className="stroke-white" />
+                </button>
+                <ul className="hidden p-1.5 group-hover:block absolute z-10 w-[93px] right-0 rounded-md shadow-[3px_3px_14px_rgba(46,66,118,0.14)] bg-white text-main-black text-sm leading-[18px]">
+                  <li className="p-1 hover:bg-[#F1F4FA] focus:bg-[#F1F4FA] focus:rounded-lg hover:rounded-lg">
+                    최신순
+                  </li>
+                  <li className="p-1 hover:bg-[#F1F4FA] focus:bg-[#F1F4FA] focus:rounded-lg hover:rounded-lg">
+                    좋아요순
+                  </li>
+                  <li className="p-1 hover:bg-[#F1F4FA] focus:bg-[#F1F4FA] focus:rounded-lg hover:rounded-lg">
+                    조회순
+                  </li>
+                  <li className="p-1 hover:bg-[#F1F4FA] focus:bg-[#F1F4FA] focus:rounded-lg hover:rounded-lg">
+                    알파벳순
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
           {totalItems === 0 ? (
             <div className="bg-[#FBFCFE] h-[calc(100vh-23rem)] flex flex-col justify-center items-center gap-2.5">
