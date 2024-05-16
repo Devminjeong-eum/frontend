5807f341e8a73794eafe2d1a865823ff033fefb5
uk11 <dlckddnrr@gmail.com>
2024-05-16 18:13:15 +0900

    fix: searchBar 수정

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -3,12 +3,29 @@
 import MagnifierSvg from '@/components/svg-component/MagnifierSvg';
 import useScroll from '@/hooks/useScroll';
 import { useRouter } from 'next/navigation';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import ClearSearchBarSvg from '../svg-component/ClearSearchBarSvg';
 
 export default function SearchBar() {
   const isScrolled = useScroll();
   const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isInputFocus, setIsInputFocus] = useState(false);
+  const searchBarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        searchBarRef.current &&
+        !searchBarRef.current.contains(e.target as HTMLElement)
+      )
+        setIsInputFocus(false);
+    };
+
+    document.addEventListener('mousedown', handleClick);
+
+    return () => document.removeEventListener('mousedown', handleClick);
+  }, []);
 
   const navigateToSearch = (): void => {
     inputRef.current?.value.trim() &&
@@ -22,25 +39,41 @@ export default function SearchBar() {
     }
   };
 
-  const handleSearchClick = () => {
-    navigateToSearch();
+  const handleInputClearAndFocus = () => {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+      inputRef.current.focus();
+    }
+  };
+
+  const handleInputFocusChange = () => {
+    setIsInputFocus(true);
   };
 
   return (
     <div
-      className={`z-10 bg-main-gradiant-bottom top-0  ${isScrolled ? 'sticky h-[64px] py-2 px-5' : 'h-[104px] p-5'}`}
+      className={`z-10 bg-main-gradiant-bottom top-0 ${isScrolled ? 'sticky h-[64px] py-2 px-5' : 'h-[104px] p-5'}`}
     >
-      <div className="relative">
+      <div className="relative" ref={searchBarRef}>
         <input
           ref={inputRef}
           type="text"
-          placeholder="궁금한 IT용어를 검색해 보세요."
-          className="w-full h-[48px] rounded-[16px] bg-white/20 ring-1 ring-white/40 focus:ring-white/60 pl-5 py-4 outline-none text-white placeholder:text-[#C8CAEB]"
+          placeholder={`${isInputFocus ? '' : '궁금한 IT용어를 검색해 보세요.'}`}
+          className={`${
+            isInputFocus
+              ? 'w-full h-[48px] rounded-[16px] ring-1 ring-white/40 pl-5 py-4 outline-none'
+              : 'w-full h-[48px] rounded-[16px] bg-white/20 ring-1 ring-white/40 focus:ring-white/60 pl-5 py-4 outline-none text-white placeholder:text-[#C8CAEB]'
+          }`}
           onKeyDown={handleSearchKeyDown}
+          onFocus={handleInputFocusChange}
         />
-        <button onClick={handleSearchClick}>
-          <MagnifierSvg className="absolute right-[20px] top-[12px]" />
-        </button>
+        {isInputFocus ? (
+          <button onClick={handleInputClearAndFocus}>
+            <ClearSearchBarSvg className="absolute right-[20px] top-[12px]" />
+          </button>
+        ) : (
+          <MagnifierSvg className="absolute right-[20px] top-[12px] cursor-pointer" />
+        )}
       </div>
     </div>
   );
