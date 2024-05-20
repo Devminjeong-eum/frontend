6ec523c19b727fc32a8172b7e170ae740bb1c5fd
nyoung113 <mysoulk0113@gmail.com>
2024-05-20 21:22:11 +0900

    fix: 제거된 await 생성

--- src/fetcher/index.ts
+++ src/fetcher/index.ts
@@ -4,7 +4,7 @@ import { DefaultRes, WordDetail } from './types.ts';
 
 export const getWordDetail = async (wordId: string) => {
   try {
-    return backendFetch<DefaultRes<WordDetail>>(`/word/${wordId}`);
+    return await backendFetch<DefaultRes<WordDetail>>(`/word/${wordId}`);
   } catch (e) {
     // NOTE: 상황에 맞는 페이지 보여줘야 함.
     console.log('error');
