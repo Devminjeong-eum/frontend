579ec9d2648995a0c6ef677029d0182c930eec86
uk11 <dlckddnrr@gmail.com>
2024-05-24 18:59:12 +0900

    fix: 문구 추가

--- src/components/pages/search/NotFoundWord.tsx
+++ src/components/pages/search/NotFoundWord.tsx
@@ -4,8 +4,9 @@ export default function NotFoundWord() {
   return (
     <div className="flex flex-col justify-center items-center pt-[200px] ">
       <BigMagnifierSvg />
-      <div className="mt-5 text-[#A8AEBC]">
-        앗! 찾으시는 검색 결과가 없어요.
+      <div className="mt-5 text-[#A8AEBC] text-center">
+        앗! 찾으시는 검색 결과가 없어요. <br />
+        영문 검색만 가능합니다.
       </div>
     </div>
   );
