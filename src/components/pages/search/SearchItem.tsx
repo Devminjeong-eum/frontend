6dcada50d59572a189c333876f6a43bf88c14f1e
uk11 <dlckddnrr@gmail.com>
2024-05-24 14:42:19 +0900

    fix: 발음, 발음 기호 한개씩 보이도록 수정

--- src/components/pages/search/SearchItem.tsx
+++ src/components/pages/search/SearchItem.tsx
@@ -31,12 +31,12 @@ export default function SearchItem({ item, searchWord }: Props) {
             {/* 단어 뜻 */}
             <div className="text-main-charcoal break-words flex-auto text-[14px] font-[400]">
               <span className="mr-[1px] text-[#E1E5ED] font-extralight">|</span>{' '}
-              {item.pronunciation}
+              {item.pronunciation[0]}
             </div>
             {/* 발음기호 */}
             <p className="text-[#858596] text-[11px] whitespace-nowrap font-[300] -ml-[1px]">
               {/* 아래 로직은 기존 데이터가 ,로 구분되어서 이렇게 해두었는데 수정될 시에 추가 수정 필요 */}
-              {item.diacritic}
+              {item.diacritic[0]}
               {/* .includes(',')
                  ? item.wordDiacritic.split(',').join(' | ')
                  : item.wordDiacritic} */}
