a3abbff67434282874d87614d63a83bdd84cc3c0
uk11 <dlckddnrr@gmail.com>
2024-05-24 12:39:21 +0900

    fix: 실시간으로 변경되는 searchParams를 받아 검색 api 요청

--- src/app/word/search/page.tsx
+++ src/app/word/search/page.tsx
@@ -1,30 +1,45 @@
+'use client';
+
 import Header from '@/components/layout/Header';
 import SearchItem from '@/components/pages/search/SearchItem';
 import NotFoundWord from '@/components/pages/search/NotFoundWord';
 import Link from 'next/link';
 import { getWordSearch } from '@/fetcher';
+import { useState, useEffect } from 'react';
+import { SearchWordData } from '@/fetcher/types';
+import { useSearchParams } from 'next/navigation';
 
-type Props = {
-  searchParams: { keyword: string };
-};
+export default function SearchPage() {
+  const [totalCount, setTotalCount] = useState(0);
+  const [data, setData] = useState<SearchWordData[]>();
+  const searchParams = useSearchParams();
+  const wordName = searchParams.get('keyword')?.toLowerCase();
 
-export default async function SearchPage({ searchParams }: Props) {
-  const wordName = decodeURI(searchParams.keyword);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (wordName) {
+        const {
+          data: { totalCount, data },
+        } = await getWordSearch(wordName);
+        setTotalCount(totalCount);
+        setData(data);
+      }
+    };
 
-  const {
-    data: { totalCount, data },
-  } = await getWordSearch(wordName);
+    fetchData();
+  }, [wordName]);
 
   return (
     <>
       <Header />
       <main className="p-5 rounded-[24px] bg-[#FBFCFE] -mt-[20px] z-50 flex flex-col gap-[8px]">
-        {!totalCount && <NotFoundWord wordName={wordName} />}
-        {data.map((item) => (
-          <Link href={`/words/${item.id}`}>
-            <SearchItem key={item.id} item={item} />
-          </Link>
-        ))}
+        {!totalCount && <NotFoundWord />}
+        {data &&
+          data.map((item) => (
+            <Link href={`/words/${item.id}`} key={item.id}>
+              <SearchItem item={item} />
+            </Link>
+          ))}
       </main>
     </>
   );
