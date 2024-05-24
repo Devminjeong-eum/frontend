c37a7d9cb5251fee6ce17d0a91b30df25f785557
uk11 <dlckddnrr@gmail.com>
2024-05-24 12:38:11 +0900

    fix: notFoundWord 수정

--- src/components/pages/search/NotFoundWord.tsx
+++ src/components/pages/search/NotFoundWord.tsx
@@ -1,15 +1,11 @@
 import BigMagnifierSvg from '@/components/svg-component/BigMagnifierSvg';
 
-type Props = {
-  wordName: string;
-};
-
-export default function NotFoundWord({ wordName }: Props) {
+export default function NotFoundWord() {
   return (
     <div className="flex flex-col justify-center items-center pt-[200px] ">
       <BigMagnifierSvg />
       <div className="mt-5 text-[#A8AEBC]">
-        앗! {wordName}에 대한 검색 결과가 없어요.
+        앗! 찾으시는 검색 결과가 없어요.
       </div>
     </div>
   );
