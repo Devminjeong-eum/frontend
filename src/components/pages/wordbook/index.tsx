ea460571544a6d437e1814dfce297e24dff6a8c7
ono212 <qkrwjdals302@naver.com>
2024-05-28 21:00:39 +0900

    style: 좋아요 단어 개수 padding-left 추가
    - 모바일에서 봤을 때 너무 왼쪽에 붙어 있어서 추가해주었습니다.

--- src/components/pages/wordbook/index.tsx
+++ src/components/pages/wordbook/index.tsx
@@ -27,7 +27,7 @@ export default function Wordbook() {
           <div className="flex">
             <div className="w-[calc(46%+30px)] h-9 overflow-hidden rounded-tl-xl relative top-px">
               <div className="bg-[#FBFCFE] [transform:rotateY(180deg)_skew(-30deg)_translate(30px,0px)] h-full rounded-tl-md flex justify-center items-center">
-                <div className="[transform:rotateY(180deg)_skew(-30deg)_translate(-40px,0px)] text-[#313140] font-medium text-sm leading-[18px] tracking-[-0.02em] opacity-90">
+                <div className="[transform:rotateY(180deg)_skew(-30deg)_translate(-40px,0px)] pl-2.5 text-[#313140] font-medium text-sm leading-[18px] tracking-[-0.02em] opacity-90">
                   총 {totalCount}개
                 </div>
               </div>
