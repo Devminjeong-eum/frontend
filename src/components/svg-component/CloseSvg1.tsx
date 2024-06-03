e0df02fa24cfab608a2e4bb7727113d581607285
uk11 <dlckddnrr@gmail.com>
2024-06-03 23:12:28 +0900

    feat: 퀴즈 공유 svg 추가

---
+++ src/components/svg-component/CloseSvg1.tsx
@@ -0,0 +1,26 @@
+export default function CloseSvg1() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18 6L6 18"
+        stroke="#222222"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6 6L18 18"
+        stroke="#222222"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
