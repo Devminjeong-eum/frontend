9d6474c6054378925c447e0f76b81791a0b77efb
nyoung113 <mysoulk0113@gmail.com>
2024-06-04 18:40:51 +0900

    refactor: copyClipboard hook 분리 및 ShareModal Kakao, clipboard 적용

--- src/components/common/ShareModal.tsx
+++ src/components/common/ShareModal.tsx
@@ -1,10 +1,18 @@
+'use client';
+
 import KakaoIconSvg from '@/components/svg-component/KakaoIconSvg.tsx';
 import LinkShareIconSvg from '@/components/svg-component/LinkShareIconSvg.tsx';
 import ModalXSvg from '@/components/svg-component/ModalXSvg.tsx';
 import Link from 'next/link';
 import { QUIZ_PATH } from '@/routes/path.ts';
+import useLoadKakaoScript from '@/hooks/useLoadKakaoScript.ts';
+import useCopyClipboard from '@/hooks/useCopyClipboard.ts';
+import { CopiedNotice } from '@/components/common/CopiedNotice.tsx';
 
 export default function ShareModal() {
+  const { handleShare } = useLoadKakaoScript();
+  const { isCopied, onCopyClipboard } = useCopyClipboard();
+
   return (
     <div className="fixed max-w-[430px] top-0 w-full h-full flex justify-center items-center bg-[#17192470] px-[46px]">
       <div className="relative w-full h-[172px] bg-white rounded-[16px] flex flex-col justify-center items-center">
@@ -16,19 +24,24 @@ export default function ShareModal() {
         </h3>
         <div className="flex justify-center gap-[37px]">
           <div className="flex flex-col items-center">
-            <div className="w-[52px] h-[52px] rounded-full bg-kakao m-2 flex justify-center items-center">
-              <KakaoIconSvg />
-            </div>
-            <span>카카오톡</span>
+            <button onClick={handleShare}>
+              <div className="w-[52px] h-[52px] rounded-full bg-kakao m-2 flex justify-center items-center">
+                <KakaoIconSvg />
+              </div>
+              <span>카카오톡</span>
+            </button>
           </div>
           <div className="flex flex-col items-center">
-            <div className="w-[52px] h-[52px] rounded-full bg-[#F2F4F9] m-2 flex justify-center items-center">
-              <LinkShareIconSvg />
-            </div>
-            <span>URL 복사</span>
+            <button onClick={onCopyClipboard}>
+              <div className="w-[52px] h-[52px] rounded-full bg-[#F2F4F9] m-2 flex justify-center items-center">
+                <LinkShareIconSvg />
+              </div>
+              <span>URL 복사</span>
+            </button>
           </div>
         </div>
       </div>
+      {isCopied && <CopiedNotice />}
     </div>
   );
 }
