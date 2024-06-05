5c9107b35ee6688851a42530742cf09a2a086dfe
ono212 <qkrwjdals302@naver.com>
2024-06-05 18:29:12 +0900

    feat: 단어검색페이지 메타데이터 추가

--- src/app/word/search/page.tsx
+++ src/app/word/search/page.tsx
@@ -1,4 +1,39 @@
+import { ResolvingMetadata } from 'next';
 import PageClient from './PageClient';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function generateMetadata(
+  { searchParams }: { searchParams: { keyword: string } },
+  parent: ResolvingMetadata,
+) {
+  const parentMetadata = (await parent) || [];
+
+  const openGraph = parentMetadata?.openGraph ?? {};
+  const twitter = parentMetadata?.twitter ?? {};
+
+  return {
+    ...parentMetadata,
+    title: {
+      absolute: `'${searchParams.keyword}'의 검색결과 | 데브말싸미`,
+    },
+    description: `'${searchParams.keyword}'에 대한 검색 결과를 확인해보세요.`,
+    robots: {
+      index: false,
+      follow: false,
+    },
+    openGraph: {
+      ...openGraph,
+      title: { absoulte: `'${searchParams.keyword}'의 검색결과 | 데브말싸미` },
+      description: `'${searchParams.keyword}'에 대한 검색 결과를 확인해보세요.`,
+      url: 'https://dev-malssami.site/quiz',
+    },
+    twitter: {
+      ...twitter,
+      title: { absoulte: `'${searchParams.keyword}'의 검색결과 | 데브말싸미` },
+      description: `'${searchParams.keyword}'에 대한 검색 결과를 확인해보세요.`,
+    },
+  };
+}
 
 export default function SearchPage() {
   return <PageClient />;
