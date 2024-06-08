f2fe2e9cf382a31b2741f26d3fad5a821440c9d8
ono212 <qkrwjdals302@naver.com>
2024-06-08 23:49:11 +0900

    fix: 불필요한 주석 제거, og url 잘못 작성한 것 수정

--- src/app/word/search/page.tsx
+++ src/app/word/search/page.tsx
@@ -1,6 +1,6 @@
 import { ResolvingMetadata } from 'next';
 import PageClient from './PageClient';
-//searchParams: { [key: string]: string | string[] | undefined }
+
 // eslint-disable-next-line react-refresh/only-export-components
 export async function generateMetadata(
   {
@@ -27,7 +27,7 @@ export async function generateMetadata(
       ...openGraph,
       title: { absoulte: `'${searchParams.keyword}'의 검색결과 | 데브말싸미` },
       description: `'${searchParams.keyword}'에 대한 검색 결과를 확인해보세요.`,
-      url: 'https://dev-malssami.site/quiz',
+      url: 'https://dev-malssami.site/search',
     },
     twitter: {
       ...twitter,
