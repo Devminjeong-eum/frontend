df826d7a3d775ea1d62da7d2ab1e4b421b467be0
nyoung113 <mysoulk0113@gmail.com>
2024-06-09 15:43:48 +0900

    feat: dtail page UI 변경 구현

---
+++ src/components/svg-component/DetailPronunciationCorrectSvg.tsx
@@ -0,0 +1,13 @@
+export default function DetailPronunciationCorrectSvg() {
+  return (
+    <svg
+      width="13"
+      height="13"
+      viewBox="0 0 13 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="6.5" cy="6.5" r="4.75" stroke="#0C3FC1" strokeWidth="1.5" />
+    </svg>
+  );
+}
