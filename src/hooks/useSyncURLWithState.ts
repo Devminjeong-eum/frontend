eaa2730a2390b784fd448aebe1c640b317fe5e4c
Ahn taeuk <dotory0829@naver.com>
2024-06-09 18:42:47 +0900

    WordItem 수정

--- src/hooks/useSyncURLWithState.ts
+++ src/hooks/useSyncURLWithState.ts
@@ -6,7 +6,7 @@ export default function useSyncURLWithState() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [currentPage, setCurrentPage] = useState(
-    Number(searchParams.get('page')!) || 1,
+    Number(searchParams.get('page')) || 1,
   );
   const [isTrending, setIsTrending] = useState<TrendingType>(
     (searchParams.get('view') as TrendingType) || 'all',
