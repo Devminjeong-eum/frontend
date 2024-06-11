c4cc2a5f989a2363ee0afdd19aaa515e896c017e
uk11 <dlckddnrr@gmail.com>
2024-06-11 22:40:09 +0900

    feat: 회원탈퇴 페이지 추가

--- src/app/profile/DeleteAccount/page.tsx
+++ src/app/profile/DeleteAccount/page.tsx
@@ -1,5 +1,23 @@
 import DeleteAccount from '@/components/pages/profile/DeleteAccount';
+import { serverFetch } from '@/fetcher/serverFetch';
+import { FetchRes, DefaultRes, UserData } from '@/fetcher/types';
+import { notFound } from 'next/navigation';
 
-export default function DeleteAccountPage() {
-  return <DeleteAccount />;
+const getUserInfo = async () => {
+  try {
+    return await serverFetch<FetchRes<DefaultRes<UserData>>>(`/user`);
+  } catch (e) {
+    console.log('error', e);
+    notFound();
+  }
+};
+
+export default async function DeleteAccountPage() {
+  const {
+    data: {
+      data: { userId },
+    },
+  } = await getUserInfo();
+
+  return <DeleteAccount userId={userId} />;
 }
