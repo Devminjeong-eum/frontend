053a9b3fc9c819e7e40df64a597d791cc646be16
uk11 <dlckddnrr@gmail.com>
2024-06-16 19:17:27 +0900

    fix: react query로 검색 데이터를 불러오도록 변경

--- src/components/pages/search/index.tsx
+++ src/components/pages/search/index.tsx
@@ -1,20 +1,29 @@
 'use client';
 
-import type { SearchWordData } from '@/fetcher/types';
+// import type { SearchWordData } from '@/fetcher/types';
 import WordItem from '@/components/common/WordItem';
 import LoginAlertModal from '@/components/common/LoginAlertModal';
 import { useState } from 'react';
+import { useGetSearchWord } from '@/hooks/query/useGetSearchWord';
+import NotFoundWord from './NotFoundWord';
 
 type Props = {
-  data: SearchWordData[];
+  word: string;
 };
 
-export default function Search({ data }: Props) {
+export default function Search({ word }: Props) {
   const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const {
+    data: {
+      data: { data, totalCount },
+    },
+  } = useGetSearchWord(word);
 
   return (
     // FIXME: 트렌딩 단어 오픈 후에는 아래 px-5 제거하기
     <div className="flex flex-col gap-[7px] mt-[17px] px-5">
+      {!totalCount && <NotFoundWord />}
       {data.map((item) => (
         <WordItem key={item.id} {...item} setIsOpenModal={setIsOpenModal} />
       ))}
