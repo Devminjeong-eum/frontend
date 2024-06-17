faaf5dbaafd774a03b34d2cbf9e4387fcb4bac4e
nyoung113 <mysoulk0113@gmail.com>
2024-06-17 15:48:28 +0900

    fix: design QA 반영 - header, 좋아요 하트 정렬

--- src/components/pages/detail/DetailHeader.tsx
+++ src/components/pages/detail/DetailHeader.tsx
@@ -5,10 +5,13 @@ import URLShareButton from '@/components/pages/detail/URLShareButton.tsx';
 
 export default function DetailHeader() {
   return (
-    <div className=" bg-[#2341BE] h-12 flex justify-between items-center border-b border-[#4152C1] px-[16px]">
-      <BackButton />
-      <h1 className="text-white">개발 용어</h1>
-      <URLShareButton />
+    <div className="bg-[#2341BE] h-12 flex flex-col px-[16px]">
+      <div className="w-full h-full flex justify-between  items-center">
+        <BackButton />
+        <h1 className="text-white">개발 용어</h1>
+        <URLShareButton />
+      </div>
+      <div className="w-full border-b border-[#4152C1]"></div>
     </div>
   );
 }
