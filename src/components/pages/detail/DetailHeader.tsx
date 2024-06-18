d39a231dbb5fe6a075e8e1d0eae93c88cf980c05
nyoung113 <mysoulk0113@gmail.com>
2024-06-18 21:15:01 +0900

    fix: 띄어쓰기 수정

--- src/components/pages/detail/DetailHeader.tsx
+++ src/components/pages/detail/DetailHeader.tsx
@@ -6,7 +6,7 @@ import URLShareButton from '@/components/pages/detail/URLShareButton.tsx';
 export default function DetailHeader() {
   return (
     <div className="bg-[#2341BE] h-12 flex flex-col px-[16px]">
-      <div className="w-full h-full flex justify-between  items-center">
+      <div className="w-full h-full flex justify-between items-center">
         <BackButton />
         <h1 className="text-white">개발 용어</h1>
         <URLShareButton />
