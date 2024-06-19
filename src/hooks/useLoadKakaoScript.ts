cba1879cf7f465efa7aadec683251d46f0f81f2d
Ahn taeuk <dotory0829@naver.com>
2024-06-19 23:32:23 +0900

    로그인 유저일 경우 카카오톡 공유하기 타이틀 및 설명 텍스트 변경

--- src/hooks/useLoadKakaoScript.ts
+++ src/hooks/useLoadKakaoScript.ts
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect } from 'react';
+import useAuthQuery from './query/useAuthQuery';
+import { DefaultRes, UserInfo } from '@/fetcher/types';
 
 declare global {
   interface Window {
@@ -9,15 +11,18 @@ declare global {
   }
 }
 
+function isDefaultRes<TData>(response: unknown): response is DefaultRes<TData> {
+  return (response as DefaultRes<TData>).data !== undefined;
+}
+
 export default function useLoadKakaoScript() {
   const { Kakao } = window;
+  const { data } = useAuthQuery();
 
-  /**
-   * NOTE: title, description을 어떻게 처리할지 고민 필요
-   * 현재는 props로 내려받는다고 가정하고 동적으로 처리되도록 구현해두었음
-   * */
-  const title = 'testTitle';
-  const desc = 'testDescription, 타이틀은 ${title}이고, 이렇게 넣으면 됨';
+  const userName = isDefaultRes<UserInfo>(data) ? data.data.name : '사용자';
+
+  const title = `${userName}님의 개발 용어 점수는?`;
+  const desc = `${userName}님의 개발 용어 점수는 몇 점일까요? 클릭해서 확인해보고, 함께 도전해보세요!`;
 
   // FIXME: 나중에 엔드포인트가 quiz/quizID와 같이 바뀔텐데 아직 정해진게 없는것 같아서 추후 수정 예정
   const urlEndPoint = window.location.href.split('/')[3];
