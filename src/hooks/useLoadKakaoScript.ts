61df8aada02b183bdc707fc4f3caeac43613edbb
Ahn taeuk <dotory0829@naver.com>
2024-06-19 23:39:20 +0900

    카카오 공유하기 path 수정

--- src/hooks/useLoadKakaoScript.ts
+++ src/hooks/useLoadKakaoScript.ts
@@ -25,9 +25,10 @@ export default function useLoadKakaoScript() {
   const desc = `${userName}님의 개발 용어 점수는 몇 점일까요? 클릭해서 확인해보고, 함께 도전해보세요!`;
 
   // FIXME: 나중에 엔드포인트가 quiz/quizID와 같이 바뀔텐데 아직 정해진게 없는것 같아서 추후 수정 예정
-  const urlEndPoint = window.location.href.split('/')[3];
+  const urlEndPoint = window.location.href.split('/').slice(3).join('/');
   const path = urlEndPoint;
 
+  console.log(path);
   useEffect(() => {
     if (!Kakao.isInitialized()) {
       Kakao.init(process.env.NEXT_PUBLIC_KAKAO_SHARE_KEY);
