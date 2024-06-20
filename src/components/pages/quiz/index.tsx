f854c3858c229c2425bf52c105e23f515a4de012
nyoung113 <mysoulk0113@gmail.com>
2024-06-21 00:47:10 +0900

    fix: quiz 결과 누적 수정(atom 초기화)

--- src/components/pages/quiz/index.tsx
+++ src/components/pages/quiz/index.tsx
@@ -5,12 +5,27 @@ import ScoreSVG from '@/components/svg-component/ScoreSvg';
 import QuizPlay from './QuizPlay';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useSetAtom } from 'jotai/react';
+import { guestQuizAtom } from '@/store';
 
 export default function Quiz() {
   const [isShow, setIsShow] = useState(false);
   const router = useRouter();
+  const setQuizAtom = useSetAtom(guestQuizAtom);
 
-  if (isShow) return <QuizPlay />;
+  const handleClickPlayButton = () => {
+    setQuizAtom(() => {
+      return {
+        correctWordData: [],
+        incorrectWordData: [],
+      };
+    });
+    setIsShow(true);
+  };
+
+  if (isShow) {
+    return <QuizPlay />;
+  }
 
   return (
     <div className="px-5 h-screen flex flex-col justify-center items-center bg-main-gradient-full relative">
@@ -31,7 +46,7 @@ export default function Quiz() {
         <p className="text-white font-medium">나의 개발 용어 발음 지식은?</p>
       </div>
       <button
-        onClick={() => setIsShow(true)}
+        onClick={handleClickPlayButton}
         className="w-[calc(100%-40px)] flex justify-center items-center absolute bottom-[50px] h-12 rounded-[16px] bg-white/20 ring-1 ring-white/40 focus:ring-white/60 outline-none text-white
         shadow-quiz
         hover:cursor-pointer"
