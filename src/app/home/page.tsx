e125ba0d3bd280d49b2b9fd0aee94765b94d9c40
Ahn taeuk <dotory0829@naver.com>
2024-06-24 20:44:57 +0900

    스켈레톤 ui 적용

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,6 @@
-import Spinner from '@/components/common/Spinner';
 import Header from '@/components/layout/Header';
 import HomeClientPage from '@/components/pages/home';
 import QUERY_KEYS from '@/constants/queryKey';
-
 import {
   HydrationBoundary,
   QueryClient,
@@ -11,6 +9,7 @@ import {
 import { Suspense } from 'react';
 import { cookies } from 'next/headers';
 import { getAllPostsServer } from '@/fetcher/server.ts';
+import HomeSkeleton from '@/components/pages/home/HomeSkeleton';
 
 export default async function HomePage({
   searchParams: { page },
@@ -29,7 +28,7 @@ export default async function HomePage({
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Header isToken={isToken} />
-      <Suspense fallback={<Spinner />}>
+      <Suspense fallback={<HomeSkeleton />}>
         <HomeClientPage />
       </Suspense>
     </HydrationBoundary>
