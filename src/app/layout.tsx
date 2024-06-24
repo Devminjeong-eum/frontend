7584910a800ed7d05d5ab3f985610498d32294ea
Ahn taeuk <dotory0829@naver.com>
2024-06-24 19:33:48 +0900

    fix: 모바일 기준 양쪽 border가 생기던 부분 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,12 +136,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="ko" className={`${gugi.variable} ${pretendard.variable}`}>
       <GoogleAnalytics gaId="G-JVH2GTHE6P" />
       <body className="font-pretendard">
         <div className="flex justify-center min-h-screen bg-[#FBFCFE] scrollbar-hide">
-          <div className="w-full max-w-[430px] border-1 border-x border-gray-200 shadow-xl ">
+          <div className="w-full max-w-[430px] shadow-xl ">
             <QueryProvider>
               {children}
               <ReactQueryDevtools initialIsOpen={false} />
