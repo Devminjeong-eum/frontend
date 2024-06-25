3875b1007896797ea0eaf238e736c3811e5cb2cc
RookieAND <gwangin1999@naver.com>
2024-06-26 00:52:19 +0900

    feat: 등수 변화를 보여주는 Component 추상화

---
+++ src/components/pages/home/trending-posts/RankChange.tsx
@@ -0,0 +1,53 @@
+import clsx from 'clsx';
+
+import FillArrowSvg from '@/components/svg-component/FillArrowSvg';
+import HyphenSvg from '@/components/svg-component/HyphenSvg';
+
+type Props = {
+  className: string;
+  innerClassName?: string;
+  rankChange?: number;
+};
+
+export const RankChange = ({
+  className,
+  innerClassName,
+  rankChange,
+}: Props) => {
+  const isNew = typeof rankChange !== 'number';
+  const isSame = !isNew && rankChange === 0;
+  const isChanged = !isNew && rankChange !== 0;
+
+  console.log({
+    isNew,
+    isSame,
+    isChanged,
+  });
+
+  return (
+    <div
+      className={clsx(
+        'mr-[22px] rounded-xl w-[34px] h-[17px] px-[3px] flex items-center justify-center',
+        className,
+      )}
+    >
+      {isNew && (
+        <p className={clsx(innerClassName, 'text-[11px] text-[#6C55F8]')}>
+          New
+        </p>
+      )}
+      {isSame && <HyphenSvg className={innerClassName} />}
+      {isChanged && (
+        <>
+          <FillArrowSvg
+            className={innerClassName}
+            transform={rankChange > 0 ? '' : 'rotate(180)'}
+          />
+          <p className={clsx(innerClassName, 'text-[11px] font-semibold')}>
+            {Math.abs(rankChange)}
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
