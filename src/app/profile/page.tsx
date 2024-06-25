b22d915c00bab0cdbea49fd00a837f8a5b416461
uk11 <dlckddnrr@gmail.com>
2024-06-25 23:42:50 +0900

    fix: route 변경

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,36 @@
+import Profile from '@/components/pages/profile';
+import { serverFetch } from '@/fetcher/serverFetch';
+import { DefaultRes, FetchRes, UserData } from '@/fetcher/types';
+import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
+
+const getUserInfo = async () => {
+  try {
+    return await serverFetch<FetchRes<DefaultRes<UserData>>>(`/user`);
+  } catch (e) {
+    console.log('error', e);
+    notFound();
+  }
+};
+
+export default async function ProfilePage() {
+  const isToken = cookies().has('accessToken');
+
+  if (!isToken) return <Profile />;
+
+  const {
+    data: {
+      data: { userId, likeCount, name, profileImage },
+    },
+  } = await getUserInfo();
+
+  return (
+    <Profile
+      userId={userId}
+      likeCount={likeCount}
+      name={name}
+      profileImage={profileImage}
+      isToken={isToken}
+    />
+  );
+}
