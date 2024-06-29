7a92ad3045529dcd09b737bbe2ad9d00b042ae70
nyoung113 <mysoulk0113@gmail.com>
2024-06-29 23:13:55 +0900

    feat: 백그라운드 클릭 시 클립보드 복사 모달이 사라짐 구현

--- src/components/common/CopiedNotice.tsx
+++ src/components/common/CopiedNotice.tsx
@@ -1,13 +1,24 @@
 import CheckSvg from '@/components/svg-component/CheckSvg.tsx';
 
-export function CopiedNotice() {
-  return (
-    <div className="w-screen h-screen fixed left-0 top-0 flex justify-center items-center z-10">
-      <div className="w-full h-screen flex justify-center items-center max-w-[430px] bg-[#000000] opacity-70 mix-blend-multiply" />
-      <div className="absolute flex gap-1.5 py-4 px-[22px] rounded-[16px] bg-[#05050555]">
-        <CheckSvg />
-        <span className="font-semibold text-white">링크 복사 완료</span>
+interface Props {
+  isOpen: boolean;
+  handleClose: () => void;
+}
+export function CopiedNotice({ isOpen, handleClose }: Props) {
+  if (isOpen) {
+    return (
+      <div
+        className="w-screen h-screen fixed left-0 top-0 flex justify-center items-center z-10"
+        onClick={handleClose}
+      >
+        <div className="w-full h-screen flex justify-center items-center max-w-[430px] bg-[#000000] opacity-70 mix-blend-multiply" />
+        <div className="absolute flex gap-1.5 py-4 px-[22px] rounded-[16px] bg-[#05050555]">
+          <CheckSvg />
+          <span className="font-semibold text-white">링크 복사 완료</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return null;
 }
