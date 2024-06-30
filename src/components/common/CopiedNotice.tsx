de11d57c9c97b414895f2d68b9ec3cd432773a47
nyoung113 <mysoulk0113@gmail.com>
2024-06-30 22:14:48 +0900

    refactor: if 문 리팩터링

--- src/components/common/CopiedNotice.tsx
+++ src/components/common/CopiedNotice.tsx
@@ -5,20 +5,18 @@ interface Props {
   handleClose: () => void;
 }
 export function CopiedNotice({ isOpen, handleClose }: Props) {
-  if (isOpen) {
-    return (
-      <div
-        className="w-screen h-screen fixed left-0 top-0 flex justify-center items-center z-10"
-        onClick={handleClose}
-      >
-        <div className="w-full h-screen flex justify-center items-center max-w-[430px] bg-[#000000] opacity-70 mix-blend-multiply" />
-        <div className="absolute flex gap-1.5 py-4 px-[22px] rounded-[16px] bg-[#05050555]">
-          <CheckSvg />
-          <span className="font-semibold text-white">링크 복사 완료</span>
-        </div>
-      </div>
-    );
-  }
+  if (!isOpen) return null;
 
-  return null;
+  return (
+    <div
+      className="w-screen h-screen fixed left-0 top-0 flex justify-center items-center z-10"
+      onClick={handleClose}
+    >
+      <div className="w-full h-screen flex justify-center items-center max-w-[430px] bg-[#000000] opacity-70 mix-blend-multiply" />
+      <div className="absolute flex gap-1.5 py-4 px-[22px] rounded-[16px] bg-[#05050555]">
+        <CheckSvg />
+        <span className="font-semibold text-white">링크 복사 완료</span>
+      </div>
+    </div>
+  );
 }
