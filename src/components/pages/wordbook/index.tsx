1d1e2bd7b74f8d078d6fbf23bb50c13c5192db2b
ono212 <qkrwjdals302@naver.com>
2024-07-03 18:39:22 +0900

    fix: 총 용어 개수 padding bottom 추가

--- src/components/pages/wordbook/index.tsx
+++ src/components/pages/wordbook/index.tsx
@@ -36,7 +36,7 @@ export default function Wordbook() {
           <div className="flex">
             <div className="w-[calc(46%+30px)] h-9 overflow-hidden rounded-tl-xl relative top-px">
               <div className="bg-[#FBFCFE] [transform:rotateY(180deg)_skew(-30deg)_translate(30px,0px)] h-full rounded-tl-md flex justify-center items-center">
-                <div className="[transform:rotateY(180deg)_skew(-30deg)_translate(-40px,0px)] pl-2.5 text-[#313140] font-medium text-[15px] leading-[18px] tracking-[-0.02em] opacity-90">
+                <div className="[transform:rotateY(180deg)_skew(-30deg)_translate(-40px,0px)] pl-2.5 pt-0.5 text-[#313140] font-medium text-[15px] leading-[18px] tracking-[-0.02em] opacity-90">
                   총 {totalCount}개
                 </div>
               </div>
