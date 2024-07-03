ed091a54fef5955a1c06d00cba4f221d1e708321
ono212 <qkrwjdals302@naver.com>
2024-07-03 20:28:07 +0900

    fix: 복사완료화면이 오른쪽으로 밀리는 현상 수정
    세로 스크롤바 width만큼 모달이 이동하고 있음 -> 그만큼 모달에 padding-right를 지정하여 밀리지 않도록 수정

--- src/components/common/CopiedNotice.tsx
+++ src/components/common/CopiedNotice.tsx
@@ -1,15 +1,34 @@
+'use client';
+
 import CheckSvg from '@/components/svg-component/CheckSvg.tsx';
+import { useEffect, useState } from 'react';
 
 interface Props {
   isOpen: boolean;
   handleClose: () => void;
 }
+
+const getScrollbarWidth = () => {
+  return window.innerWidth - document.documentElement.clientWidth;
+};
+
 export function CopiedNotice({ isOpen, handleClose }: Props) {
+  const [scrollbarWidth, setScrollbarWidth] = useState(0);
+
+  useEffect(() => {
+    if (isOpen) {
+      setScrollbarWidth(getScrollbarWidth());
+    } else {
+      setScrollbarWidth(0);
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
     <div
       className="w-screen h-screen fixed left-0 top-0 flex justify-center items-center z-10"
+      style={{ paddingRight: `${scrollbarWidth}px` }}
       onClick={handleClose}
     >
       <div className="w-full h-screen flex justify-center items-center max-w-[430px] bg-[#000000] opacity-70 mix-blend-multiply" />
