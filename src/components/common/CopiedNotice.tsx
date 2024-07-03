be0df37f527d9d02b2ebafc3c690c5f05f5b7115
ono212 <qkrwjdals302@naver.com>
2024-07-04 00:54:25 +0900

    refactor: width를 100% 바꿔도 적용되기 때문에 추가한 로직 제거하고 w-full로 지정

--- src/components/common/CopiedNotice.tsx
+++ src/components/common/CopiedNotice.tsx
@@ -1,38 +1,16 @@
-'use client';
-
 import CheckSvg from '@/components/svg-component/CheckSvg.tsx';
-import { useEffect, useState } from 'react';
 
 interface Props {
   isOpen: boolean;
   handleClose: () => void;
 }
 
-const getScrollbarWidth = () => {
-  return window.innerWidth - document.documentElement.clientWidth;
-};
-
 export function CopiedNotice({ isOpen, handleClose }: Props) {
-  const [scrollbarWidth, setScrollbarWidth] = useState(0);
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    if (isOpen) {
-      const width = getScrollbarWidth();
-      setScrollbarWidth(width);
-      setIsReady(true);
-    } else {
-      setIsReady(false);
-      setScrollbarWidth(0);
-    }
-  }, [isOpen]);
-
-  if (!isOpen || !isReady) return null;
+  if (!isOpen) return null;
 
   return (
     <div
-      className="w-screen h-screen fixed left-0 top-0 flex justify-center items-center z-10"
-      style={{ paddingRight: `${scrollbarWidth}px` }}
+      className="w-full h-screen fixed top-0 left-0 flex justify-center items-center z-10"
       onClick={handleClose}
     >
       <div className="w-full h-screen flex justify-center items-center max-w-[430px] bg-[#000000] opacity-70 mix-blend-multiply" />
