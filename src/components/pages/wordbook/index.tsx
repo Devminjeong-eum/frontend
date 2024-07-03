e8b56755d904d231cf507c2f0dfcdfb4f7ec9943
ono212 <qkrwjdals302@naver.com>
2024-07-03 18:35:51 +0900

    fix: 퀴즈 배너-검색 버튼 간격 조정, NoWordSvg와의 상하 간격 조정

--- src/components/pages/wordbook/index.tsx
+++ src/components/pages/wordbook/index.tsx
@@ -27,12 +27,12 @@ export default function Wordbook() {
   const { data: wordData, totalCount } = data.data.data;
 
   return (
-    <div>
+    <div className="bg-[#FBFCFE]">
       <div className="bg-wordbook-gradient h-32">
         <WordbookHeader />
       </div>
-      <div className="flex flex-col justify-between">
-        <div className="relative bottom-16 flex flex-col">
+      <div className="flex flex-col justify-between mt-[-4rem]">
+        <div className="flex flex-col">
           <div className="flex">
             <div className="w-[calc(46%+30px)] h-9 overflow-hidden rounded-tl-xl relative top-px">
               <div className="bg-[#FBFCFE] [transform:rotateY(180deg)_skew(-30deg)_translate(30px,0px)] h-full rounded-tl-md flex justify-center items-center">
@@ -47,7 +47,7 @@ export default function Wordbook() {
             />
           </div>
           {totalCount === 0 ? (
-            <div className="bg-[#FBFCFE] h-[calc(100vh-23rem)] flex flex-col justify-center items-center gap-2.5">
+            <div className="bg-[#FBFCFE] flex flex-col items-center gap-2.5 pt-[8.438rem] pb-[8.125rem]">
               <NoWordSvg />
               <span className="text-[#A8AEBC] font-medium text-center tracking-[-0.02em]">
                 좋아요를 누른 단어가 없어요.
@@ -72,7 +72,7 @@ export default function Wordbook() {
             </div>
           )}
         </div>
-        <div className="flex flex-col items-center gap-8 relative top-[-2rem]">
+        <div className="flex flex-col items-center gap-5 bg-[#FBFCFE] pb-7">
           {totalCount === 0 ? (
             <Link
               href={WORD_LIST_PATH}
@@ -81,13 +81,15 @@ export default function Wordbook() {
               <p className="text-[#383697] font-semibold">단어 검색하러 가기</p>
             </Link>
           ) : (
-            <Pagination
-              viewPaginationNums={4}
-              total={totalCount || 0}
-              limit={10}
-              setCurrent={setCurrentPage}
-              current={currentPage}
-            />
+            <div className="flex items-center justify-center w-10/12 h-14">
+              <Pagination
+                viewPaginationNums={4}
+                total={totalCount || 0}
+                limit={10}
+                setCurrent={setCurrentPage}
+                current={currentPage}
+              />
+            </div>
           )}
           <QuizBanner />
         </div>
