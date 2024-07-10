d6971bb4473e875267b33a1a874ca3e7f353882a
ono212 <qkrwjdals302@naver.com>
2024-07-10 22:57:35 +0900

    feat: 검색결과가 없는 페이지에  "단어 제보하러 가기" 버튼 UI 추가

--- src/components/pages/search/NotFoundWord.tsx
+++ src/components/pages/search/NotFoundWord.tsx
@@ -1,12 +1,18 @@
 import BigMagnifierSvg from '@/components/svg-component/BigMagnifierSvg';
+import Link from 'next/link';
 
 export default function NotFoundWord() {
   return (
-    <div className="flex flex-col justify-center items-center pt-[200px] ">
+    <div className="flex flex-col justify-center items-center pt-[200px]">
       <BigMagnifierSvg />
       <div className="mt-5 text-[#A8AEBC] text-center">
-        앗! 찾으시는 검색 결과가 없어요. <br />
-        영문 검색만 가능합니다.
+        앗! 찾으시는 검색 결과가 없어요.
+        {/* // TODO: 용어 제보 폼 링크 추가하기 */}
+        <Link href={''}>
+          <div className="rounded-2xl bg-[#E7EBF8] w-[212px] h-14 py-4 mt-7 text-[#383697] text-[16px] font-semibold">
+            단어 제보하러 가기
+          </div>
+        </Link>
       </div>
     </div>
   );
